29e8ff79dc54d68a5929c256fa62ef18f0f84faa
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-01-29 15:31:59 -0800

    Fixed some formatting

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,20 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-export const MovieCard = ({ movie, onMovieClick}) => {
-    console.log('MovieCard props:', movie);
-    if (!movie || !movie.title) {
-    // Handle the case when movie or movie.Title is undefined
+export const MovieCard = ({ movie, onMovieClick }) => {
+  console.log('MovieCard props:', movie);
+  if (!movie || !movie.title) {
+    // Handle the case when movie or movie.title is undefined
     return <div>Loading...</div>;
   }
-    return (
-        <div onClick={() => {
-              onMovieClick(movie);
-        }}>
-            {movie.title}
-        </div>
-    );
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
 };
 
 // defining all the props constraints for the MovieCard
@@ -22,21 +24,20 @@ export const MovieCard = ({ movie, onMovieClick}) => {
    display a warning in the console upon running the app.The prop name should be same as the one in
    MainView */
 MovieCard.propTypes = {
-    movie: PropTypes.shape ({
-        _id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string,
-        imagePath: PropTypes.string,
-        genre: PropTypes.shape ({
-            Name: PropTypes.string.isRequired,
-            description: PropTypes.string,
-        }),
-        director: PropTypes.shape ({
-            Name: PropTypes.string.isRequired,
-            bio: PropTypes.string,
-            birth: PropTypes.string
-        }),
-       
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    imagePath: PropTypes.string,
+    genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    }),
+    director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      bio: PropTypes.string,
+      birth: PropTypes.string,
+    }),
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
